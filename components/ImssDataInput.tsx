cb2e291f32aef71931a16139450e864ff1aa01c5
Jdelbarcogarza <jdelbarcogarza@gmail.com>
2024-09-23 09:19:49 -0600

    finished poc

---
+++ components/ImssDataInput.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React, { FormEvent } from "react";
+
+const ImssDataInput = () => {
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Create a new FormData object from the form element
+    const formData = new FormData(e.currentTarget);
+
+    // Extract individual values using get()
+    const nss = formData.get("nss");
+    const agregado = formData.get("agregado");
+
+      // Get the iframe element and its contentWindow
+      const iframe = document.getElementById("target-iframe") as HTMLIFrameElement;
+      if (iframe && iframe.contentWindow) {
+        // Post message to the iframe's window
+        iframe.contentWindow.postMessage({ nss, agregado }, "http://localhost:5173");
+      } else {
+        console.error("Iframe not found or contentWindow is not accessible.");
+      }
+
+    console.log({ nss, agregado });
+  };
+
+  return (
+    <form
+      className="flex gap-4 items-end border rounded-lg border-gray-500 p-4"
+      onSubmit={sendMessage}
+    >
+      <div className="space-y-2">
+        <label
+          htmlFor="nss"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          NSS
+        </label>
+        <input
+          type="text"
+          id="nss"
+          name="nss"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Escriba aqui NSS"
+          required
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label
+          htmlFor="agregado"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Agregado
+        </label>
+        <input
+          type="text"
+          id="agregado"
+          name="agregado"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Agregado"
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Enviar
+      </button>
+    </form>
+  );
+};
+
+export default ImssDataInput;
